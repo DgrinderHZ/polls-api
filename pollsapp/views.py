27676c73b35051e26162d8f63b0275f2c3b3b135
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-25 22:38:38 +0100

    Update results view

--- pollsapp/views.py
+++ pollsapp/views.py
@@ -18,8 +18,10 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = f"You're looking at the results of question {question_id}."
-    return HttpResponse(response)
+    q = get_object_or_404(Question, pk=question_id)
+    temp = 'polls/results.html'
+    ctx = {'question': q}
+    return render(request, temp, ctx)
 
 
 def vote(request, question_id):
@@ -36,6 +38,6 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
 
-        return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
 
     return HttpResponse(f"You're voting on question {question_id}.")
