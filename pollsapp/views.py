d59f6091790e139870d1a50bf46d4f5609a6b87c
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-25 22:28:08 +0100

    Update vote view

--- pollsapp/views.py
+++ pollsapp/views.py
@@ -1,6 +1,8 @@
-from pollsapp.models import Question
+from django.http.response import HttpResponseRedirect
+from pollsapp.models import Choice, Question
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 def index(request):
@@ -21,4 +23,19 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except  (KeyError, Choice.DoesNotExist):
+        ctx = {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        }
+        return render(request, 'polls/detail.html', ctx)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
+
     return HttpResponse(f"You're voting on question {question_id}.")
