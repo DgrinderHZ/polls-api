f609482330861793eb5ce48fc9b9ff78a5915309
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-25 21:28:35 +0100

    Add templates

--- pollsapp/urls.py
+++ pollsapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import detail, index, results, vote
 urlpatterns = [
     path('polls/', index, name='index'),
+    path('polls/<int:question_id>/', detail, name='detail'),
+    path('polls/<int:question_id>/results/', results, name='results'),
+    path('polls/<int:question_id>/vote/', vote, name='vote'),
+
 ]
--- pollsapp/views.py
+++ pollsapp/views.py
@@ -1,6 +1,22 @@
+from pollsapp.models import Question
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hoooola!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
