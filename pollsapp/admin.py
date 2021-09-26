910c969496d60758c9db08b37acfd0c91ce39e4e
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-26 18:41:04 +0100

    Customize the admin form

--- pollsapp/admin.py
+++ pollsapp/admin.py
@@ -2,5 +2,16 @@ from .models import Choice, Question
 from django.contrib import admin
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionModel(admin.ModelAdmin):
+    fields = ['question_text', 'pub_date']
+
+
+class ChoiceModel(admin.ModelAdmin):
+    fieldsets = [
+        ('Choice information', {'fields': ['choice_text', 'votes']}),
+        ('Question information', {'fields': ['question']}),
+    ]
+
+
+admin.site.register(Question, QuestionModel)
+admin.site.register(Choice, ChoiceModel)
