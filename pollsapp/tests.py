d590bea7bf0648771dfbd0ea6c9764489bcc7ef2
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-26 17:47:36 +0100

    Add QuestionDetaillViewtests

--- pollsapp/tests.py
+++ pollsapp/tests.py
@@ -58,7 +58,7 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
-    
+
     def test_past_question(self):
         """
         Questions with a pub_date in the past are displayed on the
@@ -70,7 +70,7 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             [question],
         )
-    
+
     def test_future_question(self):
         """
         Questions with a pub_date in the future aren't displayed on
@@ -105,3 +105,25 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             [question2, question1],
         )
+
+
+class QuestionDetaillViewtests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question('Future Question?', 5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question('Past Question.', -5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
