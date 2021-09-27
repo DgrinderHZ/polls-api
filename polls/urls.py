55c94567d9f2fe2e41816f8d5040c9ab536e46a8
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-27 14:23:34 +0100

    Start API

--- polls/urls.py
+++ polls/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='polls/index.html'), name='home'),
     path('', include('pollsapp.urls'))
 ]
