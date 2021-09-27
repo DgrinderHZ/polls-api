6125927f6a5383a48acea07f86d09e52f08b480a
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-27 22:51:05 +0100

    Move to generics

--- pollsapi/apiviews.py
+++ pollsapi/apiviews.py
@@ -1,21 +1,26 @@
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from .serializers import QuestionSerializer
+from .serializers import ChoiceSerializer, QuestionSerializer, VoteSerializer
 
 from pollsapp.models import Question, Choice
 
 
-class QuestionList(APIView):
-    def get(self, request):
-        questions = Question.objects.all()[:20]
-        data = QuestionSerializer(questions, many=True).data
-        return Response(data)
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
 
-class QuestionDetail(APIView):
-    def get(self, request, pk):
-        question = get_object_or_404(Question, pk=pk)
-        data = QuestionSerializer(question).data
-        return Response(data)
+class QuestionDetail(generics.RetrieveDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class VoteCreate(generics.CreateAPIView):
+    serializer_class = VoteSerializer
--- pollsapi/urls.py
+++ pollsapi/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .apiviews import QuestionList, QuestionDetail
+from .apiviews import ChoiceList, QuestionList, QuestionDetail, VoteCreate
 
 
 app_name = 'pollsapi'
 urlpatterns = [
-    path('api/polls/', QuestionList.as_view(), name='polls_list'),
+    path('api/polls/', QuestionList.as_view(), name='pollsapi_list'),
     path('api/polls/<int:pk>/',
-         QuestionDetail.as_view(), name='polls_detail'),
+         QuestionDetail.as_view(), name='pollsapi_detail'),
+    path('api/choices/', ChoiceList.as_view(), name='choicesapi_list'),
+    path('api/vote/', VoteCreate.as_view(), name='voteapi_create'),
 ]
