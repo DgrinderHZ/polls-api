8eff203918b799868ef08f8c3011015ff6a4678d
DgrinderHZ <hassanzekkouri@gmail.com>
2021-09-27 23:28:09 +0100

    Add more views

--- pollsapi/apiviews.py
+++ pollsapi/apiviews.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 from .serializers import ChoiceSerializer, QuestionSerializer, VoteSerializer
 
@@ -22,5 +23,26 @@ class ChoiceList(generics.ListCreateAPIView):
     serializer_class = ChoiceSerializer
 
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(APIView):
     serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get('voted_by')
+        data = {'choice': choice_pk, 'question': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            choice = get_object_or_404(Choice, pk=choice_pk)
+            choice.tally += 1
+            choice.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionChoiceList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        querset = Choice.objects.filter(question_id=self.kwargs['pk'])
+        return querset
+    serializer_class = ChoiceSerializer
--- pollsapi/urls.py
+++ pollsapi/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .apiviews import ChoiceList, QuestionList, QuestionDetail, VoteCreate
+from .apiviews import ChoiceList, QuestionList, \
+     QuestionDetail, VoteCreate, QuestionChoiceList
 
 
 app_name = 'pollsapi'
@@ -8,5 +9,7 @@ urlpatterns = [
     path('api/polls/<int:pk>/',
          QuestionDetail.as_view(), name='pollsapi_detail'),
     path('api/choices/', ChoiceList.as_view(), name='choicesapi_list'),
-    path('api/vote/', VoteCreate.as_view(), name='voteapi_create'),
+    path('api/polls/<int:pk>/choices/<int:choice_pk>/vote/', VoteCreate.as_view(), name='voteapi_create'),
+    path('api/polls/<int:pk>/choices/',
+         QuestionChoiceList.as_view(), name='pollsapi_qstn_choice_list'),
 ]
